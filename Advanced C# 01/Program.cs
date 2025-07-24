a8a59ff72a227da3b1303ea9f97531fd537090f8
Mazen Fady <mazenfady85@gmail.com>
2025-07-24 21:22:53 +0300

    Add project files.

---
+++ Advanced C# 01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Demo
+{
+    public class Program
+    {
+        #region Helper
+        //public static void Main()
+        //{
+        //    double X = 10 ,Y = 20 ;
+        //    Console.WriteLine($"X :{X} , Y :{Y} ");
+        //    Helper.swap(ref X,ref Y);
+        //    Console.WriteLine("___________________");
+        //    Console.WriteLine($"X :{X} , Y :{Y} ");
+        //}
+       
+
+       
+        //static class Helper 
+        //{
+        //    public static void swap<T>(ref T number1 , ref T number2)
+        //    {
+        //        T temp = number1;
+        //        number1 = number2;
+        //        number2 = temp;
+
+        //    }
+        //}
+        #endregion
+
+
+
+
+    }
+
+
+
+}
